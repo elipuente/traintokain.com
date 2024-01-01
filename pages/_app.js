97bd737114d2bba68b67ac88567c5e085b33cb79
Eli Puente <elipuente333@gmail.com>
2024-01-01 13:57:41 -0600

    Migrate to @vercel/speed-insights

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 import '../styles/global.css';
 
@@ -52,6 +53,7 @@ const FitnessCompetition = ({ Component, pageProps }) => {
     <ProvideUser>
       <Component {...pageProps} />
       <Analytics />
+      <SpeedInsights />
     </ProvideUser>
   );
 };
